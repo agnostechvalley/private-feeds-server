bfdd5e06ac1ef5cbd7c8b6f435a18734a8bad8bb
Erik Straub <erik@madglory.com>
2015-10-27 15:40:33 -0400

    Temporarily turn off JSON responses

--- feeds/response.js
+++ feeds/response.js
@@ -2,25 +2,32 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (req, res) {
-	res.format({
-		json: function () {
-			fs.readFile(path.join(__dirname, '/rss.json'), function (err, data) {
-				if (err) {
-					res.status(500).send(err);
-				}
-
-				res.send(data);
-			});
-		},
-
-		default: function () {
-			fs.readFile(path.join(__dirname, '/rss.xml'), function (err, data) {
-				if (err) {
-					res.status(500).send(err);
-				}
-
-				res.send(data);
-			});
+	fs.readFile(path.join(__dirname, '/rss.xml'), function (err, data) {
+		if (err) {
+			res.status(500).send(err);
 		}
+
+		res.send(data);
 	});
+	// res.format({
+	// 	json: function () {
+	// 		fs.readFile(path.join(__dirname, '/rss.json'), function (err, data) {
+	// 			if (err) {
+	// 				res.status(500).send(err);
+	// 			}
+
+	// 			res.send(data);
+	// 		});
+	// 	},
+
+	// 	default: function () {
+	// 		fs.readFile(path.join(__dirname, '/rss.xml'), function (err, data) {
+	// 			if (err) {
+	// 				res.status(500).send(err);
+	// 			}
+
+	// 			res.send(data);
+	// 		});
+	// 	}
+	// });
 };
